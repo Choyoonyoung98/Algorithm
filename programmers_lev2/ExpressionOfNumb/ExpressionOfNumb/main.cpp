3b35070d886073d3d15026da1679dc27167be663
조윤영 <yoon980208@gmail.com>
2019-08-31 17:34:05 +0900

    [예제]숫자의 표현(다른 사람 풀이 반영..)

--- programmers_lev2/ExpressionOfNumb/ExpressionOfNumb/main.cpp
+++ programmers_lev2/ExpressionOfNumb/ExpressionOfNumb/main.cpp
@@ -16,13 +16,6 @@
  
 첫 번째 숫자를 n이라고 했을 때, n*k + k(k-1)
  
-
- n
- 2n+1
- 3n+2
- 4n+3
- 
- 
  자연수 n이 매개변수로 주어질 때, 연속된 자연수들로 n을 표현하는 방법의 수를 return하는 solution를 완성해주세요.
  */
 
@@ -34,6 +27,7 @@ using namespace std;
 
 int solution(int n) {
     int answer = 0;
+    
     for(int i=1; i<n+1; i++) {
         for(int j=1; (j*i+((j-1)*j/2))<=15; j++) {
             if((j*i+((j-1)*j/2)) == 15){
@@ -45,7 +39,24 @@ int solution(int n) {
     return answer;
 }
 
+int solution2(int n) {
+    int answer = 0;
+    int sum;
+    
+    for(int i = 1; i <= n; i++) {
+        sum = 0;
+        for(int j = i; j <= n; j++) {
+            sum += j;
+            if(sum > n) break;
+            if(sum == n) answer++;
+        }
+    }
+    
+    return answer;
+}
+
 int main(int argc, const char * argv[]) {
     cout<<solution(15);
+    
     return 0;
 }
