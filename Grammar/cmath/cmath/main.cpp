d7b4634d65766cf81d4b37835eef98d361c60b1e
조윤영 <yoon980208@gmail.com>
2019-08-31 23:27:51 +0900

    [기본 문법]cmath

---
+++ Grammar/cmath/cmath/main.cpp
@@ -0,0 +1,32 @@
+//
+//  main.cpp
+//  cmath
+//
+//  Created by 조윤영 on 31/08/2019.
+//  Copyright © 2019 조윤영. All rights reserved.
+//
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    /*ceil: 가장 가까운 정수로 올림, 반환값:double*/
+    ceil(3.14); //4
+    
+    /*floor: 가장 가까운 정수로 내림, 반환값:double*/
+    floor(1.89);//1
+    
+    /*abs: 절대값 반환*/
+    abs(-123); //123
+    
+    /*pow: 제곱값 반환*/
+    pow(2,4); //2^4 = 16
+    
+    /*sqrt: 제곱근 반환(루트값)*/
+    sqrt(16); //4
+    
+    return 0;
+}
