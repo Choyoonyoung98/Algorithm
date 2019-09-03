90dc7f9d3825e80ea00369395af7d09197d935f9
조윤영 <yoon980208@gmail.com>
2019-09-03 17:27:29 +0900

    [예제]숫자의 표현 해결

--- programmers_lev2/ExpressionOfNumb/ExpressionOfNumb/main.cpp
+++ programmers_lev2/ExpressionOfNumb/ExpressionOfNumb/main.cpp
@@ -9,12 +9,15 @@
  (문제)
  Finn은 요즘 수학공부에 빠져 있습니다. 수학 공부를 하던 Finn은 자연수 n을 연속한 자연수들로 표현 하는 방법이 여러개라는 사실을 알게 되었습니다. 예를들어 15는 다음과 같이 4가지로 표현 할 수 있습니다.
  
- 1 + 2 + 3 + 4 + 5 = 15
- 4 + 5 + 6 = 15
- 7 + 8 = 15
+ 1 + 2 + 3 + 4 + 5 = 15 = 1*5 +(1+2+3+4) = 15
+ 4 + 5 + 6 = 15  = 4*3+(1+2)
+ 7 + 8 = 15 = 7*2+ 1
  15 = 15
  
-첫 번째 숫자를 n이라고 했을 때, n*k + k(k-1)
+ 20
+ 1
+ 
+첫 번째 숫자를 n이라고 했을 때, n*k + k(k-1)/2
  
  자연수 n이 매개변수로 주어질 때, 연속된 자연수들로 n을 표현하는 방법의 수를 return하는 solution를 완성해주세요.
  */
@@ -25,12 +28,13 @@
 
 using namespace std;
 
+
 int solution(int n) {
     int answer = 0;
     
     for(int i=1; i<n+1; i++) {
-        for(int j=1; (j*i+((j-1)*j/2))<=15; j++) {
-            if((j*i+((j-1)*j/2)) == 15){
+        for(int j=1; (j*i+((j-1)*j/2))<=n; j++) {
+            if((j*i+((j-1)*j/2)) == n){
                 cout<<i<<"*"<<j<<"+"<<j-1<<"*"<<j<<"/2"<<endl;
                 answer++;
             }
@@ -42,7 +46,7 @@ int solution(int n) {
 int solution2(int n) {
     int answer = 0;
     int sum;
-    
+
     for(int i = 1; i <= n; i++) {
         sum = 0;
         for(int j = i; j <= n; j++) {
@@ -51,12 +55,12 @@ int solution2(int n) {
             if(sum == n) answer++;
         }
     }
-    
+
     return answer;
 }
 
 int main(int argc, const char * argv[]) {
-    cout<<solution(15);
+    cout<<solution(20);
     
     return 0;
 }
