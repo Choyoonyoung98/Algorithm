8166e538028401122b0a3e63f8b8cf19785d9c58
조윤영 <yoon980208@gmail.com>
2019-11-24 13:46:08 +0900

    [UPDATE]readme 정리

---
+++ programmers_lev1/README.md
@@ -0,0 +1,6 @@
+# Programmers Lv.1
+
+
+#### 📌완전 탐색
+
+- 모의고사: <a href="https://github.com/Choyoonyoung98/Algorithm/blob/master/programmers_lev1/MockTest/MockTest/main.cpp">GO</a>
--- programmers_lev2/TransferWord/TransferWord/main.cpp
+++ programmers_lev2/TransferWord/TransferWord/main.cpp
@@ -1,15 +1,59 @@
-//
-//  main.cpp
-//  TransferWord
-//
-//  Created by 조윤영 on 19/10/2019.
-//  Copyright © 2019 조윤영. All rights reserved.
-//
-
+#include <string>
+#include <vector>
+#include <algorithm>
 #include <iostream>
+#define MAX_SIZE 10
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+///
+using namespace std;
+int answer = MAX_SIZE;
+int level = 0;
+int visited[MAX_SIZE] = {false};
+void dfs(string begin, string target, vector<string> &words, int index) {
+    visited[index] = true;
+    
+    
+    if(words[index] == target) {
+        answer = min(answer, level);
+        return;
+    }
+    
+    for(int i=0; i<words.size(); i++) {
+        if(i!=index && visited[i]!=true) {
+            
+            for(int j=0; j<begin.size(); j++){
+                
+                if(words[i].find(begin.erase(j,1))!= std::string::npos) {
+                    level++;
+                    dfs(words[i], target, words, i);
+                }
+            }
+        }
+    }
+    return;
+}
+int solution(string begin, string target, vector<string> words) {
+    
+    for(int i=0; i<words.size(); i++) {
+        
+        if(words[i]!=begin && visited[i]!=true) {
+            for(int j=0; j<begin.size(); j++){
+                level = 0;
+                if(words[i].find(begin.erase(j,1)) != std::string::npos) {
+                    level++;
+                    dfs(words[i], target, words,i);
+                    
+                }
+            }
+        }
+    }
+    if(answer == MAX_SIZE) answer = 0;
+    
+    return answer;
+}
+
+int main() {
+    vector<string> words {"hot","dot","dog","lot","log","cog"};
+    solution("hit","cog",words);
+    cout<<answer;
 }
