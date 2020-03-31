d6e8c8a1f255a5d074a6fecdade8bef45b7bd326
조윤영 <yoon980208@gmail.com>
2020-03-31 22:09:06 +0900

    [UPDATE]감시카메라1

---
+++ programmers_lev3/SurveillanceCamera/SurveillanceCamera/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SurveillanceCamera
+//
+//  Created by 조윤영 on 03/03/2020.
+//  Copyright © 2020 조윤영. All rights reserved.
+//
+//문제 이름: 단속 카메라
+//https://programmers.co.kr/learn/courses/30/lessons/42884
+//풀이 방법: Greedy로 접근
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
