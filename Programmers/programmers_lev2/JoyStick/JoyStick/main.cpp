4cfe1f9e29d7b44a67d50904c81a76ffd09cfbc9
조윤영 <yoon980208@gmail.com>
2020-07-18 15:49:48 +0900

    [UPDATE] 조이스틱 풀이 추가

--- Programmers/programmers_lev2/JoyStick/JoyStick/main.cpp
+++ Programmers/programmers_lev2/JoyStick/JoyStick/main.cpp
@@ -5,65 +5,50 @@
 //  Created by 조윤영 on 27/12/2019.
 //  Copyright © 2019 조윤영. All rights reserved.
 //
-//개인적으로 문제에 대한 이해가 어려운 문제
+//https://programmers.co.kr/learn/courses/30/lessons/42860
 
 #include <string>
-#include <vector>
-#include <cmath>
-#include <algorithm>
 #include <iostream>
 
 using namespace std;
 
+ int position = 0;
+int findCursorDirection(string name, string  temp) {
+    int move = 0;
+    for(move = 1; move<name.length(); move++) {
+        int searchRightIndex = (position+move) % name.length();
+        int searchLeftIndex = (position+ name.length() - move) % name.length();
+        
+        if(name[searchRightIndex] != temp[searchRightIndex]) {
+            position = searchRightIndex;
+            return move;
+        } else if(name[searchLeftIndex] != temp[searchLeftIndex] ) {
+            //여기서 들었던 의문점: position - move 해도 되지않나?
+            //그것에 대해 찾은 답: 0번째 인덱스의 경우에는 -1이 되기 때문에 이러한 마이너스 인덱스를 찾는 것을 방지하기 위해
+            position = searchLeftIndex;
+            return move;
+        }
+    }
+    return move;
+}
+
 int solution(string name) {
     int answer = 0;
-    vector<char> backWord ={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    string temp(name.length(), 'A'); //name.length()만큼 A로 채워서 초기화
     
-    vector<char> frontWord = backWord;
-    sort(backWord.begin(), backWord.end(), greater<int>());
-    
-    string initName;
-    
-    int findFromFront = -1;
-    int findFromBack = -1;
-    
-    int moveStick = 0;
-    
-    for(int i=0; i<name.size(); i++) {
+    while(true) {
+        temp[position] = name[position];
+        answer += (name[position] - 'A'> 1+'Z'- name[position] )? 'Z'+1-name[position] : name[position] - 'A';
+        if(temp == name) break; //모든 자리에 대해 계산 완료했을 경우
         
-        findFromFront = -1;
-        findFromBack = -1;
-        
-        moveStick = 0;
-        cout<<name[i];
-        
-        for(int j=0; j<frontWord.size(); j++) {
-            cout<<"j:"<<j;
-            if(name[i] == frontWord[j]) {
-                findFromFront = j;
-                cout<<"앞에서부터"<<j;
-                break;
-            }
-            if(name[i] == backWord[j]) {
-                findFromBack = j;
-                cout<<"뒤에서부터"<<j;
-                break;
-            }
-        }
-        if(findFromFront ==findFromBack) moveStick = findFromFront;
-        else{
-            moveStick = (findFromFront !=-1)?findFromFront : (findFromBack+2);
-        }
-        
-        cout<<"["<<moveStick<<"]"<<endl;
-        answer+=moveStick;
-        
+        //왼쪽으로 갈지 오른쪽으로 갈지 정하기
+        answer += findCursorDirection( name, temp);
     }
-
+   
     return answer;
 }
 
 int main(int argc, const char * argv[]) {
-    cout<<solution("ABABAAAAAAABA");
+    cout<<solution("JEROEN");
     return 0;
 }
