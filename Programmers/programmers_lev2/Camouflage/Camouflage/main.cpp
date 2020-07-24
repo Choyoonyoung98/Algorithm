18570441c9edeb96e5d64e3b317d20a55477c63b
조윤영 <yoon980208@gmail.com>
2020-07-24 12:39:33 +0900

    [UPDATE] 위장 복습(겹치지 않는 조합 만들기)

--- Programmers/programmers_lev2/Camouflage/Camouflage/main.cpp
+++ Programmers/programmers_lev2/Camouflage/Camouflage/main.cpp
@@ -13,10 +13,49 @@
 #include <map>
 
 using namespace std;
-//(바지개수+1)*(상의개수+1)+(모자개수+1) -1
+int answer = 0;
+map<string,bool> m;
+
+void dfs(int index, vector<vector<string>> clothes) {
+    answer++;
+
+    for(int j=index+1; j<clothes.size(); j++) {
+        string matchClothes = clothes[j][1];
+        if(!m[matchClothes]) {
+            m[matchClothes] = true;
+            dfs(j, clothes);
+            m[matchClothes] = false;
+        }
+    }
+}
+
+//통과하지만, 28개의 TC 중 4개가 시간초과
+int solution2(vector<vector<string>> clothes) {
+    
+    for(int i=0; i<clothes.size(); i++) {
+        answer++;
+        string currentClothes = clothes[i][1];
+        m[currentClothes] = true;
+        
+        for(int j=i+1; j<clothes.size(); j++) {
+            string matchClothes = clothes[j][1];
+            if(!m[matchClothes]) {
+                m[matchClothes] = true;
+                dfs(j, clothes);
+                m[matchClothes] = false;
+            }
+        }
+        m[currentClothes] = false;
+    }
+    
+    return answer;
+}
+
+//조합을 알아낼 때: ((바지개수+1)*(상의개수+1)*(모자개수+1)) -1
 int solution(vector<vector<string>> clothes) {
     int answer = 1;
     map<string, int> mapItem;
+    
     //map의 경우 int값이 0으로 초기화되어 있나봐
     for(int i=0; i<clothes.size(); i++) {
         string clothType = clothes[i][1];
@@ -30,72 +69,9 @@ int solution(vector<vector<string>> clothes) {
     return (answer-1);
 }
 
-//아래 풀이는 작동은 하나 시간초과와 다소의 테스트케이스를 통과하지 못함!
-//vector<string>combination;
-//map<string, string> mapItem;
-//
-//void makeCombination(vector<vector<string>>& clothes, int index, string temp){
-//    for(int i=index; i<clothes.size(); i++) {
-//        temp += to_string(i);
-//        combination.push_back(temp);
-//        makeCombination(clothes, index+1, temp);
-//        temp = temp.substr(0, temp.size()-1);
-//    }
-//}
-//bool compare(string& a, string& b) {
-//    if(a.size() != b.size())return a.length()<b.length();
-//    else return a < b;
-//}
-//int solution2(vector<vector<string>> clothes) {
-//    int answer = 0;
-//    makeCombination(clothes, 0,"");
-//    sort(combination.begin(), combination.end(), compare);
-//
-//    for(int i=0; i<combination.size(); i++) {
-//        sort(combination[i].begin(), combination[i].end());
-//    }
-//    combination.erase(unique(combination.begin(), combination.end()), combination.end());
-//
-//    for(int i=0; i<combination.size(); i++) {
-//        if(combination[i].size() == 1){
-//            for(char comb: combination[i]) {
-//                int index = comb - '0';
-//                string clothName = clothes[index][0];
-//            }
-//            answer++;
-//        }
-//        else{
-//            bool isDuplicate = false;
-//            for(char comb: combination[i]) {
-//                int index = comb - '0';
-//                string clothType = clothes[index][1];
-//                string clothName = clothes[index][0];
-//
-//
-//                if(mapItem.find(clothType) == mapItem.end())mapItem[clothType] = clothName;
-//                else {
-//                    isDuplicate = true;
-//                    break;
-//                }
-//            }
-//            if(isDuplicate == false) {
-//                for(char comb: combination[i]) {
-//                    int index = comb - '0';
-//                    string clothName = clothes[index][0];
-//
-//                }
-//                answer++;
-//            }
-//
-//            mapItem.clear();
-//        }
-//    }
-//    return answer;
-//}
-
 int main(int argc, const char * argv[]) {
     vector<vector<string>> clothes = {{"yellow_hat", "headgear"},{"blue_sunglasses","eyewear"},{"green_turban","headgear"}};
     
-    cout<<solution(clothes);
+    cout<<solution2(clothes);
     return 0;
 }
