f9fb8b90dc9bfe7ea8ba6ac6fc9ff0180a1531fb
조윤영 <yoon980208@gmail.com>
2020-12-26 15:00:14 +0900

    [UPDATE] 문자열 뒤집기 swift 풀이 업로드

---
+++ Swift/BasicExample/FlipWord1439.playground/Contents.swift
@@ -0,0 +1,34 @@
+
+var word: String = "" //입력받을 변수에 대해서 초기화 필요
+
+public func input() {
+    word = readLine()!//옵셔널을 해제해주는 이유는 readLine은 표준 입력으로 받은 걸 읽어 String?을 리턴한다고 나타나 있다. 그래서 위와 같이 옵셔널을 해제해주는 작업이 필요하다
+}
+
+public func countFlip(_ n: Int) -> Int {
+    var result: Int = 0
+    var foundN: Bool = false
+    
+    for w in word {
+        if w == String(n)[String(n).startIndex] {
+            if !foundN {
+                foundN = true
+                result += 1
+            }
+        } else if foundN {
+            foundN = false
+        }
+    }
+    return result
+}
+
+public func solution() -> Int {
+    let answer: Int = countFlip(1) < countFlip(0) ? countFlip(1) : countFlip(1)
+    return answer
+}
+
+input()
+
+print("\(solution())\n")
+//
+
